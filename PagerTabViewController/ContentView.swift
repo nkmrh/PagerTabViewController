5307db0069443e4d2207f9be719e0981404a0b65
nkmrh <nkmrh@r.recruit.co.jp>
2025-11-13 11:29:03 +0900

    init

---
+++ PagerTabViewController/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PagerTabViewController
+//
+//  Created by 中村　肇 on 2025/11/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PagerTabViewController/PagerTabViewControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PagerTabViewControllerApp.swift
+//  PagerTabViewController
+//
+//  Created by 中村　肇 on 2025/11/13.
+//
+
+import SwiftUI
+
+@main
+struct PagerTabViewControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
